26949c4607ba3fad99ab8b95f36ba7b1c547ea96
muhammadimam155 <imamaliatari@gmail.com>
2024-12-04 00:34:49 +0500

    menu and review add

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.querySelector("#close").onclick = () =>{
     document.querySelector("#search-form").classList.remove("active");
 }
 
-let swiper = new Swiper(".home-slider",{
+var swiper = new Swiper(".home-slider",{
     spaceBetween :120,
     centeredSlides :true,
     autoplay:{
@@ -29,5 +29,31 @@ let swiper = new Swiper(".home-slider",{
         clickable: true,
     },
     loop: true,
+
+})
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween :20,
+    centeredSlides :true,
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    
+    loop: true,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+    }
 
 })
