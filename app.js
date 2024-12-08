ccaed98cc8a8801b2ee10dd1e2477693633bd057
muhammadimam155 <imamaliatari@gmail.com>
2024-12-08 09:38:12 +0500

    add loader

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ menu.onclick = () =>{
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 }
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>{
+
     menu.classList.remove("active");
-  navbar.classList.remove("fa-times");
-}
+    navbar.classList.remove("fa-times");
+    section.forEach(sec =>{
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop -150 ;
+    let id = sec.getAttribute('id');
+
+  if(top => offset && top < offset + height){
+    navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href = '+id+']').classList.add('active');
+    });
+  };
+
+    });
+};
 
 document.querySelector("#search-icon").onclick = () =>{
     document.querySelector("#search-form").classList.toggle("active");
